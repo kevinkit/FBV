6277c9ca76a5a1a795ba7931d24529a1426d7cf5
Kevin Alexander Höfle <kevinahoefle@gmail.com>
2016-10-25 16:02:13 +0200

    Update fbv_package.vhd

--- fbv_package.vhd
+++ fbv_package.vhd
@@ -1,22 +1,33 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.all;
-
+use ieee.std_logic_unsigned.all;
 
 package fbv_pkg is
-    subtype Positive is Integer range 1 to Integer'high;
-    function DIG_GAIN (iData, Factor:std_logic_vector; Decimals, res_width: Positiv ) return std_logic_vector;
+    --subtype Positive is Integer range 1 to Integer'high;
+    function DIG_GAIN (iData, Factor:std_logic_vector; Decimals, res_width: POSITIVE ) return std_logic_vector;
     procedure DIG_OFFSET (signal iDATA, Offset : in  std_logic_vector;
                           signal Result        : out std_logic_vector);
-end fbv_pgk;
+end fbv_pkg;
 
 
 package body fbv_pkg is
-    function DIG_GAIN (iData, Factor:std_logic_vector; Decimals, res_width: POSITIVE ) return std_logic_vector is
-    begin    
-        --Function must be declared here
-        
-    end function DIG_GAIN;
+    --returns iData * Factor
+
+
+
+function DIG_GAIN (iData, Factor:std_logic_vector; Decimals, res_width: POSITIVE ) return std_logic_vector is
+    variable x : std_logic_vector ((1 + iData'left) downto 0) := (others=>'0');
+    variable max : std_logic_Vector (res_width downto 0) := (others => '1');
+begin    
+    x := iData*Factor;
+    if x(x'left) = '1' then
+        return max;
+    else
+        max := x((x'left+1) downto ((x'left+1) - res_width));
+        return max;
+    end if;
+end DIG_GAIN;
     
     
     
@@ -25,39 +36,30 @@ package body fbv_pkg is
     --         2) Wenn Offset + iData größer ist als das maximum -> maximum Wert anehmen
     --            -> Wenn -Ofsset + iData kleiner 0 sind muss der Wert auf 0 gesetzt werden
     procedure DIG_OFFSET (signal iDATA, Offset : in  std_logic_vector;
-                          signal Result        : out std_logic_vector) is
-                          variable ONES_OUT : std_logic_vector(iData'range +1) := (others=>'1'); --mit 0 initialisieren (wenn Wert dann negativ, kann dieser gleich verwendet werden(?)
-                          variable Offset_resized : std_logic_vector(iData'range) := (others=>'1');
-                          variable INVERTER    : std_logic_vector(iData'range +1) := (others=>'1');
-                          variable temp_result : std_logic_vector(iData'range +1) := (others=>'0');
+                          signal Result        : out std_logic_vector) is           
+                          variable Offset_resized : std_logic_vector((iData'left) downto iData'right) := (others=>'0');
+                          variable temp_result : std_logic_vector((iData'left +1) downto iData'right) := (others=>'0');
+                          variable f_result : std_logic_vector((iData'left +1) downto iData'right) := (others =>'0');
     begin
-           --Vorzeichenbit übertragen
-           Offset_resized'high <= Offset'high;
-           
-           --Nachdem das Vorzeichen übertragen würde, die Daten an die "richtige" Stelle übertragen
-           Offset_resized(Offset'range) <= Offset; --Ob das funktioniert?
-           
-           
-           --Checken ob iData und Offset jeweils um die Häflte größer sind als ONES_OUT --> wenn ja muss die Additon nicht durchgeführt werden
-           --stattedessen einfach den maximal wert ausgeben!
-           
-           
-           
-           temp_result <= iData + signed(Offset_resized);
-           
-           if temp_result < signed(0) then
-                Result <= ZERO_OUT xor INVERTER;
+
+           --negativer Offset          
+           if Offset(Offset'left) = '1' then
+            Offset_resized := (others=>'1');
+            Offset_resized((Offset'left) downto 0) := '1' & Offset(Offset'left -1 downto 0);          
+           else
+            --Positiver Offset
+            Offset_resized(Offset_resized'left downto 0) := Offset(Offset'left-1 downto 0);
+            f_result := '0' & f_result(f_result'left - 1 downto f_result'right);
            end if;
-           --Result <= signed_idat + 
-           --Procedure must be declared here   
-           --Check if negative
-           case Offset'high is
-                when 1 =>
-                    --Result = not(ZERO_OUT);
-                when 0 =>
-            end case;
+
+           --Berechnung
+           temp_result := iData + Offset_resized; 
            
-           
-           
+           if temp_result(temp_result'left downto temp_result'left -1) = "00" then --00 bedeutet, dass es weder eine Bereichsunter oder überschreitung gab
+                Result <= temp_result;
+           else
+                Result <= f_result;
+           end if;
+
     end DIG_OFFSET;       
 end fbv_pkg;

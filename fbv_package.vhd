3488772f9d76ce5d2234148e0ca8595c7509db9a
Kevin Alexander Höfle <kevinahoefle@gmail.com>
2016-11-01 23:02:31 +0100

    Update fbv_package.vhd

--- fbv_package.vhd
+++ fbv_package.vhd
@@ -28,11 +28,14 @@ package body fbv_pkg is
 
 function DIG_GAIN (iData, Factor:std_logic_vector; Decimals, res_width: POSITIVE ) return std_logic_vector is
     variable zero_dec : std_logic_vector (Decimals-1 downto 0) := (others => '0');
-    variable Resultat_Gleitkomma : std_logic_Vector(iData'left + Decimals + Factor'left +1 downto 0)  := (others=>'0');
+  --  variable Resultat_Gleitkomma : std_logic_Vector(iData'left + Decimals + Factor'left +1 downto 0)  := (others=>'0');
+    
+    
+    variable Resultat_Gleitkomma : std_logic_Vector(iData'left + Factor'left +1 downto 0)  := (others=>'0');
     variable max : std_logic_Vector (res_width downto 0) := (others => '1');
 begin    
-    Resultat_Gleitkomma := (iData & zero_dec) *Factor;
-
+    --Resultat_Gleitkomma := (iData & zero_dec) *Factor;
+    Resultat_Gleitkomma := iData*Factor;
     if Resultat_Gleitkomma(Resultat_Gleitkomma'left downto (res_width + Decimals)) > 0 then
         --Überlauf!
         return max;
@@ -62,14 +65,17 @@ end DIG_GAIN;
             f_result := (others=>'0');
            else
             --Positiver Offset
-            Offset_resized(Offset'left -1 downto 0) := Offset(Offset'left-1 downto 0);
+            Offset_resized(Offset'left downto 0) := '0' & Offset(Offset'left-1 downto 0);
+            
+            
+            --01111.... <- maximaler POSITIVER Wert
             f_result := '0' & f_result(f_result'left - 1 downto f_result'right);
            end if;
 
            --Berechnung
            temp_result := ('0' & iData) + Offset_resized; 
            
-           if temp_result(temp_result'left downto temp_result'left -1) = "00" then --00 bedeutet, dass es weder eine Bereichsunter oder überschreitung gab
+           if temp_result(temp_result'left) ='0' then --00 bedeutet, dass es weder eine Bereichsunter oder überschreitung gab
                 Result <= temp_result;
            else
                 Result <= f_result;
